8248a74507cebb4e5ce4d6c1216a66a37ecab628
Ariel Malka <ariel@chronotext.org>
2013-01-20 20:46:02 +0200

    AudioLoop IMPLEMENTATION FOR COCOA

--- src/chronotext/sound/AudioLoop.h
+++ src/chronotext/sound/AudioLoop.h
@@ -1,44 +1,11 @@
 #pragma once
 
-#include <AudioUnit/AudioUnit.h>
+#include "cinder/Cinder.h"
 
-class AudioLoopDelegate
-{
-public:
-    virtual void renderCallback(int bufferSize, float *bufferData, double timeStamp) {}
-};
-
-class AudioLoop
-{
-    AudioComponentInstance audioUnit;
-    bool initialized;
-    
-    static OSStatus staticRenderCallback(void *inRefCon, AudioUnitRenderActionFlags *ioActionFlags, const AudioTimeStamp *inTimeStamp, UInt32 inBusNumber, UInt32 inNumberFrames, AudioBufferList *ioData)
-    {
-        AudioLoop *instance = (AudioLoop*)inRefCon;
-        if (instance->delegate)
-        {
-            float *bufferData = (float*)ioData->mBuffers[0].mData;
-            double timeStamp = inTimeStamp->mSampleTime / 44100.0;
-            
-            instance->delegate->renderCallback(inNumberFrames, bufferData, timeStamp);
-        }
-        
-        return noErr;
-    }
-    
-    void renderCallback(int bufferSize, float *bufferData, double timeStamp);
-
-public:
-    AudioLoopDelegate *delegate;
-
-    AudioLoop() : delegate(NULL), initialized(false) {}
-
-    bool init();
-    void shutdown();
-    
-    void start();
-    void stop();
-    
-    void setVolume(float volume);
-};
+#if defined(CINDER_COCOA)
+    #include "chronotext/sound/AudioLoopImplCocoa.h"
+    typedef AudioLoopImplCocoa AudioLoop;
+#elif defined(CINDER_MSW)
+    #include "chronotext/sound/AudioLoopImplMsw.h"
+    typedef AudioLoopImplMsw AudioLoop;
+#endif
