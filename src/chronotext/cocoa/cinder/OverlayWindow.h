f924c85e2831b6f268e9da335d2abe4818107d01
Ariel Malka <ariel@chronotext.org>
2013-10-04 00:51:10 +0300

    COCOA: OverlayWindow, TO BE LAYED-OUT ON TOP OF CINDER (OPEN-GL) WINDOW

---
+++ src/chronotext/cocoa/cinder/OverlayWindow.h
@@ -0,0 +1,44 @@
+#import <Cocoa/Cocoa.h>
+#import <objc/runtime.h>
+
+#import "cinder/app/AppImplCocoaRendererGl.h"
+
+/*
+ * THE IDEA IS TO RESIZE OUR OverlayWindow BEFORE THE "RESIZE SIGNAL" IS SENT
+ * THIS WOULD NOT NOT BE POSSIBLE TO ACHIEVE, SAY, USING NSWindowDidResizeNotification
+ *
+ * REFERENCES:
+ * https://github.com/cinder/Cinder/blob/c894b2a81eb4d859070b177f989f60b470e92b8c/src/cinder/app/CinderView.mm#L197
+ * http://stackoverflow.com/a/8971179/50335
+ */
+
+@interface AppImplCocoaRendererGl (ResizeHack)
+
+- (void) overridenDefaultResize;
+
+@end
+
+@implementation AppImplCocoaRendererGl (ResizeHack)
+
+- (void) overridenDefaultResize
+{
+    [self overridenDefaultResize]; // NO, IT WON'T LEAD TO A STACK-OVERFLOW :)
+    
+    NSRect frame = [cinderView.window convertRectToScreen:cinderView.bounds];
+    NSWindow *overlayWindow = [[cinderView.window childWindows] objectAtIndex:0];
+    [overlayWindow setFrame:frame display:YES];
+}
+
++ (void) load
+{
+    method_exchangeImplementations(class_getInstanceMethod(self, @selector(defaultResize)), class_getInstanceMethod(self, @selector(overridenDefaultResize)));
+}
+
+@end
+
+@interface OverlayWindow : NSWindow
+
+- (id) initWithParentWindow:(NSWindow*)window;
+- (BOOL) canBecomeKeyWindow;
+
+@end
---
+++ src/chronotext/cocoa/cinder/OverlayWindow.mm
@@ -0,0 +1,44 @@
+#import "OverlayWindow.h"
+
+@implementation OverlayWindow
+
+/*
+ * REFERENCES:
+ * https://forum.libcinder.org/topic/placing-an-nsview-above-the-cinderview
+ * http://stackoverflow.com/questions/15753313/resizing-an-nswindow-setup-as-a-childwindow
+ * http://stackoverflow.com/questions/4946342/why-nswindow-without-stylemasknstitledwindowmask-can-not-be-keywindow
+ */
+
+- (id) initWithParentWindow:(NSWindow*)window
+{
+    if (self = [super initWithContentRect:[window convertRectToScreen:[window.contentView bounds]] styleMask:NSBorderlessWindowMask backing:NSBackingStoreBuffered defer:NO])
+    {
+        self.backgroundColor = [NSColor clearColor];
+        [self setOpaque:NO];
+        [self setAutorecalculatesKeyViewLoop:YES];
+        
+        [window addChildWindow:self ordered:NSWindowAbove];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(windowWillClose:) name:NSWindowWillCloseNotification object:window];
+    }
+    
+    return self;
+}
+
+- (void) dealloc
+{
+    [super dealloc];
+}
+
+- (BOOL) canBecomeKeyWindow
+{
+    [self.parentWindow becomeKeyWindow];
+    return YES;
+}
+
+- (void) windowWillClose:(NSNotification*)notification
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    [self close]; // WILL RELEASE THE NSWindow (AND ALL THE NSView'S INSIDE)
+}
+
+@end
