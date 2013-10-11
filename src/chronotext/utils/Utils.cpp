5ef90be1dbf2fb924f798fc8f2f8ceb0dc2f83e7
arielm <ariel@chronotext.org>
2013-10-11 18:13:10 +0200

    REFACTORING: Utils MOVED TO chr NAMESPACE

--- src/chronotext/utils/Utils.cpp
+++ src/chronotext/utils/Utils.cpp
@@ -14,28 +14,31 @@ using namespace std;
 using namespace ci;
 using namespace utf8;
 
-string wstringToUtf8(const wstring &s)
+namespace chronotext
 {
-    vector<char> tmp;
-
+    string wstringToUtf8(const wstring &s)
+    {
+        vector<char> tmp;
+        
 #if defined(CINDER_MSW)
-    unchecked::utf16to8(s.data(), s.data() + s.size(), back_inserter(tmp));
+        unchecked::utf16to8(s.data(), s.data() + s.size(), back_inserter(tmp));
 #else
-    unchecked::utf32to8(s.data(), s.data() + s.size(), back_inserter(tmp));
+        unchecked::utf32to8(s.data(), s.data() + s.size(), back_inserter(tmp));
 #endif
+        
+        return string(tmp.data(), tmp.size());
+    }
     
-    return string(tmp.data(), tmp.size());
-}
-
-wstring utf8ToWstring(const string &s)
-{
-    vector<wchar_t> tmp;
-
+    wstring utf8ToWstring(const string &s)
+    {
+        vector<wchar_t> tmp;
+        
 #if defined(CINDER_MSW)
-    unchecked::utf8to16(s.data(), s.data() + s.size(), back_inserter(tmp));
+        unchecked::utf8to16(s.data(), s.data() + s.size(), back_inserter(tmp));
 #else
-    unchecked::utf8to32(s.data(), s.data() + s.size(), back_inserter(tmp));
+        unchecked::utf8to32(s.data(), s.data() + s.size(), back_inserter(tmp));
 #endif
-    
-    return wstring(tmp.data(), tmp.size());
+        
+        return wstring(tmp.data(), tmp.size());
+    }
 }
--- src/chronotext/utils/Utils.h
+++ src/chronotext/utils/Utils.h
@@ -12,7 +12,6 @@
 #include "cinder/Utilities.h"
 
 #include <fstream>
-//#include <iostream>
 
 #include <boost/algorithm/string.hpp>
 
@@ -24,157 +23,161 @@
 #include "chronotext/utils/SyslogStringStream.h"
 #endif
 
-#if defined(CINDER_MSW)
-static cinder::msw::dostream *CHROUT;
-#elif defined(CINDER_ANDROID)
-static cinder::android::dostream *CHROUT;
-#elif defined(CINDER_MAC) && defined(FORCE_SYSLOG)
-static chr::mac::dostream *CHROUT;
-#endif
-
-static inline std::ostream& chrout()
+namespace chronotext
 {
 #if defined(CINDER_MSW)
-    if (!CHROUT)
-    {
-        CHROUT = new cinder::msw::dostream;
-    }
-    return *CHROUT;
+    static ci::msw::dostream *CHROUT;
 #elif defined(CINDER_ANDROID)
-    if (!CHROUT)
-    {
-        CHROUT = new cinder::android::dostream;
-    }
-    return *CHROUT;
+    static ci::android::dostream *CHROUT;
 #elif defined(CINDER_MAC) && defined(FORCE_SYSLOG)
-    if (!CHROUT)
-    {
-        CHROUT = new chr::mac::dostream;
-    }
-    return *CHROUT;
-#else
-    return std::cout;
+    static mac::dostream *CHROUT;
 #endif
-}
-
+    
+    static inline std::ostream& chrout()
+    {
+#if defined(CINDER_MSW)
+        if (!CHROUT)
+        {
+            CHROUT = new ci::msw::dostream;
+        }
+        return *CHROUT;
+#elif defined(CINDER_ANDROID)
+        if (!CHROUT)
+        {
+            CHROUT = new ci::android::dostream;
+        }
+        return *CHROUT;
+#elif defined(CINDER_MAC) && defined(FORCE_SYSLOG)
+        if (!CHROUT)
+        {
+            CHROUT = new mac::dostream;
+        }
+        return *CHROUT;
+#else
+        return std::cout;
+#endif
+    }
+    
 #define LOGI chrout()
-
+    
 #if defined(DEBUG) || defined(FORCE_LOG)
 #define LOGD chrout()
 #else
 #define LOGD false && chrout()
 #endif
-
-// ---
-
-/*
- * cout REDIRECTION, AS DESCRIBED IN http://www.cplusplus.com/reference/iostream/ios/rdbuf/
- */
-
-static std::streambuf *gLogBackup = NULL;
-static std::ofstream gLogFileStream;
-
-static void logToFile(const ci::fs::path &filePath)
-{
-    if (!gLogBackup)
-    {
-        gLogFileStream.open(filePath.string().c_str());
-        gLogBackup = chrout().rdbuf();
-        
-        std::streambuf *psbuf = gLogFileStream.rdbuf();
-        chrout().rdbuf(psbuf);
-    }
-}
-
-static void logToConsole()
-{
-    if (gLogBackup)
-    {
-        chrout().rdbuf(gLogBackup);
-        gLogFileStream.close();
-        gLogBackup = NULL;
-    }
-}
-
-// ---
-
-static std::string hexDump(const char *data, int size)
-{
-    std::stringstream s;
-    s << std::hex;
-
-    for (int i = 0; i < size; i++)
-    {
-        s << std::setfill('0') << std::setw(2) << (*data++ & 0xff) << " ";
-    }
     
-    return s.str();
-}
-
-// ---
-
-/*
- * REFERENCE: http://stackoverflow.com/a/10096779/50335
- */
-
-static std::string prettyBytes(uint64_t numBytes, int precision = 2)
-{
-    const char *abbrevs[] = { "TB", "GB", "MB", "KB" };
-    const size_t numAbbrevs = sizeof(abbrevs) / sizeof(abbrevs[0]);
-
-    uint64_t maximum = pow(1024, numAbbrevs);
+    // ---
     
-    for (size_t i = 0; i < numAbbrevs; ++i)
+    /*
+     * cout REDIRECTION, AS DESCRIBED IN http://www.cplusplus.com/reference/iostream/ios/rdbuf/
+     */
+    
+    static std::streambuf *gLogBackup = NULL;
+    static std::ofstream gLogFileStream;
+    
+    static void logToFile(const ci::fs::path &filePath)
     {
-        if (numBytes > maximum)
+        if (!gLogBackup)
         {
-            std::stringstream s;
-            s << std::fixed << std::setprecision(precision) << numBytes / (double)maximum << " " << abbrevs[i];
-            return s.str();
+            gLogFileStream.open(filePath.string().c_str());
+            gLogBackup = chrout().rdbuf();
+            
+            std::streambuf *psbuf = gLogFileStream.rdbuf();
+            chrout().rdbuf(psbuf);
+        }
+    }
+    
+    static void logToConsole()
+    {
+        if (gLogBackup)
+        {
+            chrout().rdbuf(gLogBackup);
+            gLogFileStream.close();
+            gLogBackup = NULL;
+        }
+    }
+    
+    // ---
+    
+    static std::string hexDump(const char *data, int size)
+    {
+        std::stringstream s;
+        s << std::hex;
+        
+        for (int i = 0; i < size; i++)
+        {
+            s << std::setfill('0') << std::setw(2) << (*data++ & 0xff) << " ";
         }
         
-        maximum /= 1024;
+        return s.str();
     }
     
-    std::stringstream s;
-    s << numBytes << " Bytes";
-    return s.str();
-}
-
-// ---
-
-template<typename T>
-static int search(T *array, T value, int min, int max)
-{
-    int mid = (min + max) >> 1;
+    // ---
     
-    while (min < mid)
+    /*
+     * REFERENCE: http://stackoverflow.com/a/10096779/50335
+     */
+    static std::string prettyBytes(uint64_t numBytes, int precision = 2)
     {
-        if (array[mid - 1] < value)
+        const char *abbrevs[] = { "TB", "GB", "MB", "KB" };
+        const size_t numAbbrevs = sizeof(abbrevs) / sizeof(abbrevs[0]);
+        
+        uint64_t maximum = pow(1024, numAbbrevs);
+        
+        for (size_t i = 0; i < numAbbrevs; ++i)
         {
-            min = mid;
+            if (numBytes > maximum)
+            {
+                std::stringstream s;
+                s << std::fixed << std::setprecision(precision) << numBytes / (double)maximum << " " << abbrevs[i];
+                return s.str();
+            }
+            
+            maximum /= 1024;
         }
-        else if (array[mid - 1] > value)
-        {
-            max = mid;
-        }
-        else
-        {
-            min = max = mid;
-        }
-        mid = (min + max) >> 1;
+        
+        std::stringstream s;
+        s << numBytes << " Bytes";
+        return s.str();
     }
     
-    return mid - 1;
+    // ---
+    
+    template<typename T>
+    static int search(T *array, T value, int min, int max)
+    {
+        int mid = (min + max) >> 1;
+        
+        while (min < mid)
+        {
+            if (array[mid - 1] < value)
+            {
+                min = mid;
+            }
+            else if (array[mid - 1] > value)
+            {
+                max = mid;
+            }
+            else
+            {
+                min = max = mid;
+            }
+            mid = (min + max) >> 1;
+        }
+        
+        return mid - 1;
+    }
+    
+    template<typename T>
+    static inline int search(const std::vector<T> &array, float value, int min, int max)
+    {
+        return search((T*)array.data(), value, min, max);
+    }
+    
+    // ---
+    
+    std::string wstringToUtf8(const std::wstring &s);
+    std::wstring utf8ToWstring(const std::string &s);
 }
 
-template<typename T>
-static inline int search(const std::vector<T> &array, float value, int min, int max)
-{
-    return search((T*)array.data(), value, min, max);
-}
-
-// ---
-
-std::string wstringToUtf8(const std::wstring &s);
-std::wstring utf8ToWstring(const std::string &s);
+namespace chr = chronotext;
