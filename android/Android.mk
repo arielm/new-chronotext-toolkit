534ccc2f78f6ebd295d4d379bcd337e698a6d4a8
Ariel Malka <ariel@chronotext.org>
2013-10-29 13:06:48 +0200

    INCUBATOR/UI: UPDATING Android.mk

--- android/Android.mk
+++ android/Android.mk
@@ -41,6 +41,7 @@ endif
 ifdef CHR_USE_INCUBATOR_UI
     LOCAL_SRC_FILES += $(CHR_SRC)/chronotext/incubator/ui/Button.cpp
     LOCAL_SRC_FILES += $(CHR_SRC)/chronotext/incubator/ui/Container.cpp
+    LOCAL_SRC_FILES += $(CHR_SRC)/chronotext/incubator/ui/Fill.cpp
     LOCAL_SRC_FILES += $(CHR_SRC)/chronotext/incubator/ui/HorizontalLayout.cpp
     LOCAL_SRC_FILES += $(CHR_SRC)/chronotext/incubator/ui/HyperTextBox.cpp
     LOCAL_SRC_FILES += $(CHR_SRC)/chronotext/incubator/ui/Icon.cpp
