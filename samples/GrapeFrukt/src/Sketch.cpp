ff643fbea4d881a38063325a84fdb9a6cc333301
Ariel Malka <ariel@chronotext.org>
2014-02-01 17:29:19 +0200

    samples/GrapeFrukt: USING SAFETYDANK/S ImageSourceFileStbImage FOR SOLVING THE "PREMULTIPLIED PNG CURSE" ON iOS

--- samples/GrapeFrukt/src/Sketch.cpp
+++ samples/GrapeFrukt/src/Sketch.cpp
@@ -2,6 +2,10 @@
 
 #include "chronotext/utils/GLUtils.h"
 
+#if defined(CINDER_COCOA_TOUCH)
+#include "ImageSourceFileStbImage.h"
+#endif
+
 using namespace std;
 using namespace ci;
 using namespace chr;
@@ -23,7 +27,17 @@ void Sketch::setup(bool renewContext)
     }
     else
     {
-        animation = unique_ptr<Animation>(new Animation(make_shared<TextureAtlas>(textureManager, "MonocleMan.xml", true), InputSource::getResource("sheets.xml"), InputSource::getResource("animations.xml"), FPS));
+#if defined(CINDER_COCOA_TOUCH)
+        /*
+         * ImageSourceFileStbImage IS NECESSARY IN ORDER TO SOLVE THE "PREMULTIPLIED PNG CURSE" ON iOS
+         * REQUIRES THE FOLLOWING "USER-DEFINED SETTING" IN THE iOS XCode PROJECT:
+         * IPHONE_OPTIMIZE_OPTIONS = "-skip-PNGs";
+         */
+        ImageIoRegistrar::registerSourceType("png", ImageSourceFileStbImage::createRef, 1);
+#endif
+        
+        auto atlas = make_shared<TextureAtlas>(textureManager, "MonocleMan.xml", true);
+        animation = unique_ptr<Animation>(new Animation(atlas, InputSource::getResource("sheets.xml"), InputSource::getResource("animations.xml"), FPS));
     }
     
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
---
+++ src/chronotext/utils/stb/ImageSourceFileStbImage.cpp
@@ -0,0 +1,97 @@
+/*
+ * FROM SAFETYDANK'S FORK OF CINDER:
+ * https://github.com/safetydank/Cinder/blob/cbd00824554251d5b5a803133d616b7f21d4b8d5/src/cinder/ImageSourceFileStbImage.cpp
+ */
+
+#include "ImageSourceFileStbImage.h"
+#include "cinder/Utilities.h"
+
+namespace cinder {
+
+//  stb IO implemented with IStream
+static int _Read(void *user, char *data, int size)
+{
+	IStream* stream = static_cast<IStream*>(user);
+	return stream->readDataAvailable(data, size);
+}
+
+static void _Skip(void *user, unsigned n)
+{
+	IStream* stream = static_cast<IStream*>(user);
+    stream->seekRelative(n);
+}
+
+static int _Eof(void *user)
+{
+	IStream* stream = static_cast<IStream*>(user);
+    return stream->isEof();
+}
+
+// ----------------------------------------------------------
+
+ImageSourceFileStbImageRef ImageSourceFileStbImage::createFileFreeImageRef( DataSourceRef dataSourceRef, ImageSource::Options options )
+{
+	return ImageSourceFileStbImageRef( new ImageSourceFileStbImage( dataSourceRef, options ) );
+}
+
+ImageSourceFileStbImage::ImageSourceFileStbImage( DataSourceRef dataSourceRef, ImageSource::Options options )
+	: ImageSource(), mData(0)
+{
+    stbi_io_callbacks io;
+    io.read = _Read;
+    io.skip = _Skip;
+    io.eof  = _Eof;
+
+	IStreamRef stream = dataSourceRef->createStream();
+
+    mData = stbi_load_from_callbacks(&io, stream.get(), &mWidth, &mHeight, &mComponents, 0);
+    setDataType(ImageIo::UINT8);
+    switch (mComponents)
+    {
+        case 1:
+            setColorModel( ImageIo::CM_GRAY );
+            setChannelOrder( ImageIo::Y );
+            break;
+        case 2:
+            setColorModel( ImageIo::CM_GRAY );
+            setChannelOrder( ImageIo::YA );
+            break;
+        case 3:
+            setColorModel( ImageIo::CM_RGB );
+            setChannelOrder( ImageIo::RGB );
+            break;
+        case 4:
+            setColorModel( ImageIo::CM_RGB );
+            setChannelOrder( ImageIo::RGBA );
+            break;
+        default:
+            break;
+    }
+}
+
+void ImageSourceFileStbImage::load( ImageTargetRef target )
+{
+	// get a pointer to the ImageSource function appropriate for handling our data configuration
+	ImageSource::RowFunc func = setupRowFunc( target );
+	
+	for( int32_t row = 0; row < mHeight; ++row ) {
+        const uint8_t *data = mData + (row * mWidth * mComponents);
+		((*this).*func)( target, row, data );
+	}
+}
+
+void ImageSourceFileStbImage::registerSelf()
+{
+	const int32_t SOURCE_PRIORITY = 3;
+	ImageIoRegistrar::registerSourceGeneric( ImageSourceFileStbImage::createRef, SOURCE_PRIORITY );
+}
+
+ImageSourceFileStbImage::~ImageSourceFileStbImage()
+{
+	if (mData) {
+        stbi_image_free(mData);
+        mData = 0;
+	}
+}
+
+} // namespace ci
