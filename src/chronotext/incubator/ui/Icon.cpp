b1b931f8abedf18c83568f03e041e308e3864e11
Ariel Malka <ariel@chronotext.org>
2014-02-09 18:08:07 +0200

    Icon: ALLOWING USAGE OF “TEXTURES STRETCHED TO POWER-OF-TWO”

--- src/chronotext/incubator/ui/Icon.cpp
+++ src/chronotext/incubator/ui/Icon.cpp
@@ -17,8 +17,8 @@ namespace chronotext
     texture(texture),
     scale(scale)
     {
-        this->width = (width < 0) ? texture->getWidth() : width;
-        this->height = (height < 0) ? width = texture->getHeight() : height;
+        this->width = (width < 0) ? texture->getWidth() * texture->getMaxU() : width;
+        this->height = (height < 0) ? width = texture->getHeight() * texture->getMaxV() : height;
     }
     
     float Icon::getWidth()
