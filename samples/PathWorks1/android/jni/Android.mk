8d922a411070e0ef722895e64210a08c465f909b
Ariel Malka <ariel@chronotext.org>
2014-03-03 01:49:59 +0200

    sample/PathWorks1: iOS AND ANDROID VERSIONS

---
+++ samples/PathWorks1/android/jni/Android.mk
@@ -0,0 +1,26 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+CINDER_PATH = ../../../../../../
+
+LOCAL_SRC_FILES := main.cpp
+
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../src
+FILE_LIST := $(wildcard $(LOCAL_PATH)/../../src/*.cpp)
+LOCAL_SRC_FILES += $(FILE_LIST:$(LOCAL_PATH)/%=%)
+
+include $(LOCAL_PATH)/$(CINDER_PATH)/blocks/new-chronotext-toolkit/android/Android.mk
+
+LOCAL_CFLAGS := -DCHR_COMPLEX
+LOCAL_CFLAGS += -ffast-math -O3
+#LOCAL_CFLAGS += -g -DDEBUG
+
+LOCAL_LDLIBS := -llog -landroid
+LOCAL_STATIC_LIBRARIES := cinder boost_system boost_filesystem boost_thread freeimage ft2 android_native_app_glue
+
+LOCAL_MODULE := PathWorks1
+include $(BUILD_SHARED_LIBRARY)
+
+$(call import-module,android/native_app_glue)
+$(call import-module,cinder)
+$(call import-module,boost)
---
+++ samples/PathWorks1/ios/Classes/AppDelegate.h
@@ -0,0 +1,14 @@
+/*
+ * STUB FOR RUNNING A CinderSketch ON iOS
+ */
+
+#import <Foundation/Foundation.h>
+
+#import "CinderDelegate.h"
+
+@interface AppDelegate : CinderDelegate <UIApplicationDelegate>
+{
+    UIWindow *window;
+}
+
+@end
