59030a949fc4d687773540e72d0e659bbc6a66d1
Ariel Malka <ariel@chronotext.org>
2014-03-03 03:33:41 +0200

    STARTING WITH samples/PathWorks2

---
+++ samples/PathWorks2/src/Sketch.h
@@ -0,0 +1,46 @@
+/*
+ * THE NEW CHRONOTEXT TOOLKIT: https://github.com/arielm/new-chronotext-toolkit
+ * COPYRIGHT (C) 2012-2014, ARIEL MALKA ALL RIGHTS RESERVED.
+ *
+ * THE FOLLOWING SOURCE-CODE IS DISTRIBUTED UNDER THE MODIFIED BSD LICENSE:
+ * https://github.com/arielm/new-chronotext-toolkit/blob/master/LICENSE.md
+ */
+
+#pragma once
+
+#include "chronotext/cinder/CinderSketch.h"
+#include "chronotext/texture/TextureManager.h"
+#include "chronotext/font/xf/FontManager.h"
+#include "chronotext/path/SplinePath.h"
+#include "chronotext/path/FXGDocument.h"
+
+class Sketch : public chr::CinderSketch
+{
+    chr::TextureManager textureManager;
+    chr::TextureRef dotTexture;
+    
+    chr::xf::FontManager fontManager;
+    std::shared_ptr<chr::XFont> font;
+    
+    chr::SplinePath spline1;
+    chr::FollowablePath path1;
+    
+    chr::SplinePath spline2;
+    chr::FollowablePath path2;
+    
+    FXGDocument document;
+
+    float scale;
+    float position1;
+    float position2;
+    
+public:
+    Sketch(void *context, void *delegate = NULL);
+    
+    void setup(bool renewContext);
+    void resize();
+    void update();
+    void draw();
+    
+    void drawDots(const chr::SplinePath &spline);
+};
