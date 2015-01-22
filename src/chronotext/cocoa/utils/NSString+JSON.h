ecdfade9b7fae59dd76bbb7382b36f3f8477d8c4
Ariel Malka <ariel@chronotext.org>
2015-01-22 18:46:06 +0200

    develop: OBJECTIVE-C ENHANCEMENTS

--- src/chronotext/cocoa/utils/NSString+JSON.h
+++ src/chronotext/cocoa/utils/NSString+JSON.h
@@ -1,6 +1,6 @@
 /*
  * THE NEW CHRONOTEXT TOOLKIT: https://github.com/arielm/new-chronotext-toolkit
- * COPYRIGHT (C) 2012, ARIEL MALKA ALL RIGHTS RESERVED.
+ * COPYRIGHT (C) 2012-2015, ARIEL MALKA ALL RIGHTS RESERVED.
  *
  * THE FOLLOWING SOURCE-CODE IS DISTRIBUTED UNDER THE SIMPLIFIED BSD LICENSE:
  * https://github.com/arielm/new-chronotext-toolkit/blob/master/LICENSE.md
@@ -10,6 +10,9 @@
 
 @interface NSString (JSON)
 
-- (id) toJSON;
++ (NSString*) stringWithJSONObject:json;
+
+- (id) toJSONObject;
+- (id) toMutableJSONObject;
 
 @end
